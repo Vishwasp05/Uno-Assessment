de196b75e125575be8598cc70fa37f3260346392
sharma-vishwas <vishwasp@protonmail.com>
2024-11-10 10:45:44 +0530

    Initial Commit

---
+++ Uno/UnoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnoApp.swift
+//  Uno
+//
+//  Created by Vishwas Sharma on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct UnoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
